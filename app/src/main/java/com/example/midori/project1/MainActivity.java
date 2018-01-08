bc4a6234a2c83f1bdf930d96bacb5b42ca9400ab
midori-hust <phanthaobk@gmail.com>
2018-01-08 21:39:53 +0900

    15.Date Picker Time Picker

--- app/src/main/java/com/example/midori/project1/MainActivity.java
+++ app/src/main/java/com/example/midori/project1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.midori.project1;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -23,44 +26,54 @@ import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
+import static android.icu.util.Calendar.getInstance;
+
 public class MainActivity extends AppCompatActivity {
-    private Spinner spnCategory;
+    private DatePicker datePicker;
+    private TimePicker timePicker;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spinner);
-        spnCategory = (Spinner) findViewById(R.id.spnCategory);
-        List<String> listLanguage = new ArrayList<>();
-        listLanguage = Arrays.asList("Java","Android","PHP","C#","ASP NET");
+        setContentView(R.layout.activity_datespickertimespicker);
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line,listLanguage);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
 
-        spnCategory.setAdapter(arrayAdapter);
-
-        spnCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,spnCategory.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
+        initDatePicker();
+        initTimePicker();
 
     }
+    public void initDatePicker(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(calendar.YEAR);
+        final int month = calendar.get(calendar.MONTH);
+        final int day = calendar.get(calendar.DAY_OF_MONTH);
 
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, year+ "-"+(monthOfYear+1)+"-"+ dayOfMonth, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void initTimePicker(){
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this,hourOfDay+":"+minute,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
 
 }
