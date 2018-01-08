c73aa926d0d91d1c931c87e41fadaf05533c3d43
midori-hust <phanthaobk@gmail.com>
2018-01-08 20:39:21 +0900

    14.Spinner in Android

--- app/src/main/java/com/example/midori/project1/MainActivity.java
+++ app/src/main/java/com/example/midori/project1/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.inputmethod.BaseInputConnection;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -19,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,61 +29,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private RadioButton r_btn_gender_male;
-    private RadioButton r_btn_gender_female;
+public class MainActivity extends AppCompatActivity {
+    private Spinner spnCategory;
 
-    private CheckBox cb_hobby_1;
-    private CheckBox cb_hobby_2;
-    private CheckBox cb_hobby_3;
-    private CheckBox cb_hobby_4;
-
-    private ImageButton img_btn_submit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        initWidget();
-        img_btn_submit.setOnClickListener(this);
+        setContentView(R.layout.activity_spinner);
+        spnCategory = (Spinner) findViewById(R.id.spnCategory);
+        List<String> listLanguage = new ArrayList<>();
+        listLanguage = Arrays.asList("Java","Android","PHP","C#","ASP NET");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line,listLanguage);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice);
+
+        spnCategory.setAdapter(arrayAdapter);
+
+        spnCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,spnCategory.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
     }
 
-    public void initWidget() {
-        r_btn_gender_male = (RadioButton) findViewById(R.id.r_btn_gender_male);
-        r_btn_gender_female = (RadioButton) findViewById(R.id.r_btn_gender_female);
 
-        cb_hobby_1 = (CheckBox) findViewById(R.id.cb_hobby_1);
-        cb_hobby_2 = (CheckBox) findViewById(R.id.cb_hobby_2);
-        cb_hobby_3 = (CheckBox) findViewById(R.id.cb_hobby_3);
-        cb_hobby_4 = (CheckBox) findViewById(R.id.cb_hobby_4);
-
-        img_btn_submit = (ImageButton) findViewById (R.id.img_btn_submit);
-    }
-
-    @Override
-
-    public void onClick(View v) {
-        String slogan = " My profile : ";
-
-        if (cb_hobby_1.isChecked()) {
-            slogan += cb_hobby_1.getText().toString() + " - ";
-        }
-        if (cb_hobby_2.isChecked()) {
-            slogan += cb_hobby_2.getText().toString() + " - ";
-        }
-        if (cb_hobby_3.isChecked()) {
-            slogan += cb_hobby_3.getText().toString() + " - ";
-        }
-        if (cb_hobby_4.isChecked()) {
-            slogan += cb_hobby_4.getText().toString() + " - ";
-        }
-        if (r_btn_gender_female.isChecked()) {
-            slogan += r_btn_gender_female.getText().toString() + " ";
-        } else {
-            slogan += r_btn_gender_male.getText().toString() + " ";
-        }
-
-        Toast.makeText(MainActivity.this, slogan, Toast.LENGTH_SHORT).show();
-    }
 
 }
