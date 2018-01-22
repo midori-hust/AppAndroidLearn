c714c8e5d25f9568ce611fbad1a5693fe0e874a7
midori-hust <phanthaobk@gmail.com>
2018-01-23 00:04:39 +0900

    20.Learn:Send data between Activity

--- app/src/main/java/com/example/midori/project1/ReceiveDataActivity.java
+++ app/src/main/java/com/example/midori/project1/ReceiveDataActivity.java
@@ -1,17 +1,31 @@
 package com.example.midori.project1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 /**
  * Created by midori on 2018/01/22.
  */
 
 public class ReceiveDataActivity extends Activity {
+    private TextView tvUserInfo;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_data);
+        tvUserInfo = (TextView)findViewById(R.id.tv_user_info);
+        Intent intent = getIntent();
+        if(intent != null){
+            Bundle bundle = intent.getBundleExtra(SendDataActivity.BUNDLE);
+            if(bundle != null){
+                tvUserInfo.setText("Hello " +bundle.getString(SendDataActivity.USERNAME));
+            }else{
+                tvUserInfo.setText("Hello " +intent.getStringExtra(SendDataActivity.USERNAME));
+            }
+        }
     }
 }
--- app/src/main/java/com/example/midori/project1/SendDataActivity.java
+++ app/src/main/java/com/example/midori/project1/SendDataActivity.java
@@ -6,25 +6,53 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by midori on 2018/01/22.
  */
 
 public class SendDataActivity extends Activity{
-    public Button btn_send_data;
+    public Button btnSendData;
+    public EditText edtUsername;
+    public EditText edtPassword;
+
+    public static final String USERNAME ="username";
+    public static final String PASSWORD ="password";
+    public static final String BUNDLE="bundle";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_data);
-        btn_send_data = (Button) findViewById(R.id.btn_send_data);
-        btn_send_data.setOnClickListener(new View.OnClickListener() {
+        btnSendData = (Button) findViewById(R.id.btn_send_data);
+        edtUsername = (EditText) findViewById(R.id.edt_username);
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+
+        btnSendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SendDataActivity.this, ReceiveDataActivity.class);
-                startActivity(intent);
+                String username = edtUsername.getText().toString();
+                String password = edtPassword.getText().toString();
+                //byExtras(username,password);
+                byBundle(username,password);
             }
         });
+    }
+
+    public void byExtras(String username, String password){
+        Intent intent = new Intent(SendDataActivity.this, ReceiveDataActivity.class);
+        intent.putExtra(USERNAME,username);
+        intent.putExtra(PASSWORD,password);
+        startActivity(intent);
+    }
+
+    public void byBundle(String username, String password){
+        Intent intent = new Intent(SendDataActivity.this, ReceiveDataActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString(USERNAME,username);
+        bundle.putString(PASSWORD,password);
+        intent.putExtra(BUNDLE,bundle);
+        startActivity(intent);
     }
 }
